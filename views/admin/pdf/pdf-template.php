8766f93a5f5551f221284b5c59bd73422a6c8cc8
Chris Hardie <chris@chrishardie.com>
2020-05-04 16:06:07 -0400

    WIP: CSS: standardize some style definitions

--- views/admin/pdf/pdf-template.php
+++ views/admin/pdf/pdf-template.php
@@ -14,7 +14,7 @@ defined( 'WPINC' ) || die;
 ?>
 
 <style>
-	h3{
+	.ppn-article-title {
 		font-family: 'Lora-Bold', serif;
 		font-size: 16pt;
 		text-align: left;
@@ -25,16 +25,16 @@ defined( 'WPINC' ) || die;
 		font-family: 'Roboto', sans-serif;
 	}
 
-	.author{
+	.ppn-author {
 		color: rgb(102, 102, 102);
 		font-family: 'Roboto', sans-serif;
 	}
 
-	.author strong{
+	.ppn-author strong {
 		font-family: 'Roboto-Bold', sans-serif;
 	}
 
-	.date{
+	.ppn-date {
 		color: #666;
 		line-height: 100%;
 	}
@@ -53,30 +53,36 @@ defined( 'WPINC' ) || die;
 		text-align: center;
 	}
 
+	.ppn-article-bottom-border {
+		border-top: 1px dashed #d7d7d7;
+	}
+
 </style>
 
 <?php foreach ( $posts_to_include as $item ) : ?>
-	<?php if ( $item['image'] ) : ?>
-		<p><img src="<?php echo esc_url( $item['image'] ); ?>" alt=""></p>
-	<?php endif; ?>
-	<h3><?php echo esc_html( $item['title'] ); ?></h3>
-	<?php if ( ! empty( $item['author'] ) || ! empty( $item['date'] ) ) : ?>
-		<p class="meta">
-			<?php if ( ! empty( $item['author'] ) ) : ?>
-				<span class="author"><?php _e( 'By', 'printable-pdf-newspaper' ); ?> <strong><?php echo esc_html( $item['author'] ); ?></strong></span><br />
-			<?php endif; ?>
-			<?php if ( ! empty( $item['date'] ) ) : ?>
-				<span class="date"><?php echo esc_html( $item['date'] ); ?></span>
-			<?php endif; ?>
-		</p>
-	<?php endif; ?>
-	<div class="<?php echo $item['has_excerpt'] ? 'excerpt' : 'content'; ?>">
-		<?php echo wp_kses_post( $item['content'] ); ?>
-		<?php if ( $item['permalink'] ) : ?>
-			<br /><span class="permalink-text">&nbsp;&nbsp;&nbsp;<?php esc_attr_e( 'Continue&nbsp;Reading', 'printable-pdf-newspaper' ); ?>:</span><br/>
-			<img src="https://chart.googleapis.com/chart?chs=50x50&chld=M|1&cht=qr&chl=<?php echo esc_url( $item['permalink'] ); ?>"
-				 class="permalink-qr-code-image" width="50" height="50" />
+	<div class="ppn-article-wrapper">
+		<?php if ( $item['image'] ) : ?>
+			<p><img class="ppn-article-image" src="<?php echo esc_url( $item['image'] ); ?>" alt=""></p>
 		<?php endif; ?>
+		<h3 class="ppn-article-title"><?php echo esc_html( $item['title'] ); ?></h3>
+		<?php if ( ! empty( $item['author'] ) || ! empty( $item['date'] ) ) : ?>
+			<p class="meta ppn-meta">
+				<?php if ( ! empty( $item['author'] ) ) : ?>
+					<span class="author ppn-author"><?php _e( 'By', 'printable-pdf-newspaper' ); ?> <strong><?php echo esc_html( $item['author'] ); ?></strong></span><br />
+				<?php endif; ?>
+				<?php if ( ! empty( $item['date'] ) ) : ?>
+					<span class="date ppn-date"><?php echo esc_html( $item['date'] ); ?></span>
+				<?php endif; ?>
+			</p>
+		<?php endif; ?>
+		<div class="<?php echo $item['has_excerpt'] ? 'excerpt' : 'content'; ?>">
+			<?php echo wp_kses_post( $item['content'] ); ?>
+			<?php if ( $item['permalink'] ) : ?>
+				<br /><span class="permalink-text">&nbsp;&nbsp;&nbsp;<?php esc_attr_e( 'Continue&nbsp;Reading', 'printable-pdf-newspaper' ); ?>:</span><br/>
+				<img src="https://chart.googleapis.com/chart?chs=50x50&chld=M|1&cht=qr&chl=<?php echo esc_url( $item['permalink'] ); ?>"
+					 class="permalink-qr-code-image" width="50" height="50" />
+			<?php endif; ?>
+		</div>
+		<p class="ppn-article-bottom-border"></p>
 	</div>
-	<p style="border-top: 1px dashed #d7d7d7;"></p>
 <?php endforeach; ?>
